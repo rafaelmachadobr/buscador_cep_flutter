19120a3b3252aa4873361ef122d5b97d2ed0aeaa
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-08-14 14:44:20 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:buscador_cep_flutter/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
